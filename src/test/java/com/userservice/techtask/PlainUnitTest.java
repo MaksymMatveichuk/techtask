345b70c892c2e29fb04068ffcb450bc6d129971b
NewEnergyCode <matveychuk.m@gmail.com>
2024-06-24 17:07:53 +0300

    added Parallel Workflow

--- src/test/java/com/userservice/techtask/PlainUnitTest.java
+++ src/test/java/com/userservice/techtask/PlainUnitTest.java
@@ -26,7 +26,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceFastTest {
+public class PlainUnitTest {
 
   @Mock
   private AgeValidator ageValidator;
