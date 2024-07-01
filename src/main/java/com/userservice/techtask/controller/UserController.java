38a9882d6d7ebbcb15dc56d13be56673bba72048
NewEnergyCode <matveychuk.m@gmail.com>
2024-07-01 17:10:26 +0300

    test3

--- src/main/java/com/userservice/techtask/controller/UserController.java
+++ src/main/java/com/userservice/techtask/controller/UserController.java
@@ -7,6 +7,8 @@ import jakarta.validation.Valid;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +28,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   private final UserService userService;
+
+  @GetMapping("/health")
+  public ResponseEntity<String> healthCheck() {
+    return new ResponseEntity<>("Healthy", HttpStatus.OK);
+  }
 
   /**
    * Endpoint for creating a new user.
